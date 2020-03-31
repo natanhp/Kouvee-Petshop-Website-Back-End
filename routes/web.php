079c3b4acfc386e8becfc8330da389621878db7e
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-01 01:48:18 +0700

    Add route for product restock search by id

--- routes/web.php
+++ routes/web.php
@@ -125,7 +125,7 @@ $router->group(['prefix' => 'api/v1'], function () use ($router) {
     $router->group(['prefix' => 'productrestock', 'middleware' => ['jwt.auth', 'only.owner']], function () use ($router) {
         $router->get('getall', 'ProductRestockController@getAll');
         $router->post('insert', 'ProductRestockController@insert');
-        // $router->get('getbyid/{id}', 'SuppliersController@getSupplierById');
+        $router->get('getbyid/{id}', 'ProductRestockController@getProductRestockById');
         // $router->get('getbyname/{name}', 'SuppliersController@getSupplierByName');
         // $router->put('update', 'SuppliersController@update');
         // $router->delete('delete/{id}/{ownerId}', 'SuppliersController@delete');
