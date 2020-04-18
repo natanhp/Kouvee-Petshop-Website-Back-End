ff12c98822a2dcee4295aa2e2a666885de147937
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-19 03:57:59 +0700

    Create FCM model

---
+++ app/FCM.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int $id
+ * @property int $employee_id
+ * @property string $created_at
+ * @property string $updated_at
+ * @property string $token
+ * @property Employee $employee
+ */
+class FCM extends Model
+{
+    /**
+     * The table associated with the model.
+     * 
+     * @var string
+     */
+    protected $table = 'fcm';
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['employee_id', 'created_at', 'updated_at', 'token'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function employee()
+    {
+        return $this->belongsTo('App\Employee', 'employee_id');
+    }
+}
