f2c047341ff24b251c21b67d6db0e9475f8a3f6f
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-04-20 07:24:18 +0700

    Add function for sending downstream message

--- app/Listeners/SendRestockNotification.php
+++ app/Listeners/SendRestockNotification.php
@@ -2,7 +2,8 @@
 
 namespace App\Listeners;
 
-use App\Events\ExampleEvent;
+use App\Events\ProductMinReached;
+use App\FCMModel;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use LaravelFCM\Message\OptionsBuilder;
@@ -30,11 +31,47 @@ class SendRestockNotification implements ShouldQueue
      */
     public function handle(ProductMinReached $product_min_reached)
     {
-        $product_quantity = $product_min_reached->product->productQuantity;
-        $product_min_quantity = $product_min_reached->product->minimumQty;
+        $product = $product_min_reached->product;
+        $product_quantity = $product->productQuantity;
+        $product_min_quantity = $product->minimumQty;
+        $product_name = $product->productName;
+        $product_metric = $product->meassurement;
 
         if($product_quantity < $product_min_quantity) {
-        // TODO = Send fcm downstream message
+            $this->sendMultipleDevices($product_name, $product_quantity, $product_metric);
         }
+    }
+
+    private function sendMultipleDevices($product_name, $product_stock, $product_metric) {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setCollapseKey('a_collapse_key');
+
+        $notificationBuilder = new PayloadNotificationBuilder("$product_name Hampir Habis");
+        $notificationBuilder->setBody("Stok produk sekarang $product_stock $product_metric")
+                            ->setSound('default');
+
+        $option = $optionBuilder->build();
+        $notification = $notificationBuilder->build();
+
+        // You must change it to get your tokens
+        $tokens = FCMModel::pluck('token')->toArray();
+
+        $downstreamResponse = FCM::sendTo($tokens, $option, $notification);
+
+        $downstreamResponse->numberSuccess();
+        $downstreamResponse->numberFailure();
+        $downstreamResponse->numberModification();
+
+        // return Array - you must remove all this tokens in your database
+        $downstreamResponse->tokensToDelete();
+
+        // return Array (key : oldToken, value : new token - you must change the token in your database)
+        $downstreamResponse->tokensToModify();
+
+        // return Array - you should try to resend the message to the tokens in the array
+        $downstreamResponse->tokensToRetry();
+
+        // return Array (key:token, value:error) - in production you should remove from your database the tokens present in this array
+        $downstreamResponse->tokensWithError();
     }
 }
