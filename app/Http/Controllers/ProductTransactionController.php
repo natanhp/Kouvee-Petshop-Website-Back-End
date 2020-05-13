fd8d890302320f90ca89678da6b5ba3af449b9ea
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-14 01:26:29 +0700

    Add insert function

--- app/Http/Controllers/ProductTransactionController.php
+++ app/Http/Controllers/ProductTransactionController.php
@@ -6,6 +6,7 @@ use App\ProductTransaction;
 use App\ProductTransactionDetail;
 use App\Product;
 use App\Employee;
+use App\Events\ProductMinReached;
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
@@ -20,7 +21,7 @@ class ProductTransactionController extends Controller {
 
     /**
     * @OA\Get(
-	*     path="/api/v1/producttransaction/getall",
+	*     path="/api/v1/producttransaction/kasir/getall",
 	*	  tags={"product transaction"},
     *     security={
     *         {"bearerAuth": {}}
@@ -55,6 +56,112 @@ class ProductTransactionController extends Controller {
             "message" => "success",
             "data" => $product_transactions
         ], 200);
-	}
+    }
+    
+    /**
+     * @OA\Post(
+     *     path="/api/v1/producttransaction/cs/insert",
+     *     tags={"product transaction"},
+     *     summary="Insert a new product transaction",
+     *     @OA\Response(
+     *         response=400,
+     *         description="Error"
+     *     ),
+     *     security={
+     *         {"bearerAuth": {}}
+     *     },
+     *     @OA\RequestBody(
+     *         description="Input data format",
+     *         @OA\MediaType(
+     *             mediaType="application/json",
+     *             @OA\Schema(
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="productTransactionDetails",
+     *                     description="Array of Product Restock Detail",
+     *                     type="string",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="createdBy",
+     *                     description="The id of the owner",
+     *                     type="integer",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="total",
+     *                     description="The total price",
+     *                     type="double",
+     *                 )
+     *             )
+     *         )
+     *     )
+     * )
+     */
+    public function insert(Request $request) {
+
+        $this->validate($request, [
+            'createdBy' => 'required|numeric',
+            'productTransactionDetails' => 'required',
+            'total' => 'required|numeric'
+        ]);
+
+        $productTransactionDetails = $request->productTransactionDetails;
+        foreach($productTransactionDetails as $item) {
+            $product = Product::find($item['Products_id']);
+            
+            if($product->productQuantity - $item['itemQty'] < 0) {
+                return response()->json([
+                    "message" => "$product->productName habis",
+                    "data" => []
+                ], 400);
+            }
+        }
+
+        $latest_id = ProductTransaction::latest()->withTrashed()->first();
+        $current_id;
+        $date = Carbon::parse(Carbon::now()->toDateString())->format('dmy');
+        if ($latest_id == null) {
+            $current_id = 'PR-'.$date.'-'.'00';    
+        } else {
+            $current_id = 'PR-'.$date.'-'.sprintf("%02d", substr($latest_id->id, strrpos($latest_id->id, '-') + 1) + 1);
+        }
+
+        $product_transaction = new ProductTransaction;
+        $product_transaction->id = $current_id;
+        $product_transaction->createdBy = $request->createdBy;
+        $product_transaction->isPaid = 0;
+        $product_transaction->total = $request->total;
+        $product_transaction->Customers_id = $request->Customers_id;
+
+
+        if($product_transaction->save()) {
+            foreach($productTransactionDetails as $item) {
+                $product_transaction_detail = new ProductTransactionDetail;
+                $product_transaction_detail->itemQty = $item['itemQty'];
+                $product_transaction_detail->Products_id = $item['Products_id'];
+                $product_transaction_detail->createdBy = $item['createdBy'];
+                $product_transaction_detail->ProductTransaction_id = $current_id;
+                
+                $product = Product::find($item['Products_id']);
+                $product->productQuantity -= $item['itemQty'];
+                $product->updatedBy = $item['createdBy'];
+                $product->save();
+
+                event(new ProductMinReached($product));
+
+                $product_transaction_detail->save();
+            }
+
+         
+            return response()->json([
+                "message" => "Product transaction created",
+                "data" => [$product_transaction]
+            ], 200);
+        }
+
+        return response()->json([
+            "message" => "Product transaction not created",
+            "data" => []
+        ], 400);
+    }
 }
 ?>
