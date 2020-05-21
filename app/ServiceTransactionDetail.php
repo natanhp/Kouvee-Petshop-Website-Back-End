a1a3756156a2afab59ed15f15749c680d57b7097
Natan Hari Pamungkas <natanhp@protonmail.com>
2020-05-21 20:37:07 +0700

    Add timestamp and soft delete

--- app/ServiceTransactionDetail.php
+++ app/ServiceTransactionDetail.php
@@ -3,16 +3,18 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property int $id
- * @property string $isDeleted
+ * @property string $isFinished
  * @property string $createdAt
  * @property string $updatedAt
  * @property int $ServiceDetails_id
  * @property int $createdBy
  * @property int $updatedBy
  * @property string $ServiceTransaction_id
+ * @property string $deletedAt
  * @property ServiceDetail $serviceDetail
  * @property ServiceTransaction $serviceTransaction
  * @property Employee $employee
@@ -20,17 +22,25 @@ use Illuminate\Database\Eloquent\Model;
  */
 class ServiceTransactionDetail extends Model
 {
+    use SoftDeletes;
+    use \Askedio\SoftCascade\Traits\SoftCascadeTrait;
+
     /**
      * The table associated with the model.
      * 
      * @var string
      */
     protected $table = 'ServiceTransactionDetail';
+    protected $softCascade = ['serviceTransactionsCreatedBy'];
+
+    const CREATED_AT = 'createdAt';
+    const UPDATED_AT = 'updatedAt';
+    const DELETED_AT = 'deletedAt';
 
     /**
      * @var array
      */
-    protected $fillable = ['isDeleted', 'createdAt', 'updatedAt', 'createdBy', 'updatedBy'];
+    protected $fillable = ['isFinished', 'createdAt', 'updatedAt', 'createdBy', 'updatedBy', 'deletedAt'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
